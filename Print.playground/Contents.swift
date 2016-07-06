69ddf5c092160b06a84d8dbc7871f5cd9e602299
rebeccabartels <rebeccakbartels@icloud.com>
2016-07-06 08:49:04 -0400

    string interpolation and stuffs donezo

--- Print.playground/Contents.swift
+++ Print.playground/Contents.swift
@@ -6,14 +6,14 @@ print(favoriteWord)
 print(favoriteColor)
 print(favoriteMusician)
 
-print("I'm printing a string in Swift!")
+print("I'm printing a string in Swift!") // printing a string
 
-print("Hi Mom! My favorite word is serendipity, my favorite color is green, and my favorite musician is Billy Joel.")
+print("Hi Mom! My favorite word is serendipity, my favorite color is green, and my favorite musician is Billy Joel.") //I don't like Billy Joel
 
 let color = "blue"
-print("The sky is \(color).")
+print("The sky is \(color).") //string interpolation
 
-print("Hi Mom! My favorite word is \(favoriteWord), my favorite color is \(favoriteColor), and my favorite musician is \(favoriteMusician).")
+print("Hi Mom! My favorite word is \(favoriteWord), my favorite color is \(favoriteColor), and my favorite musician is \(favoriteMusician).") //more string interpolation
 
 var skySentence = "The sky is \(color)."
 print(skySentence)
